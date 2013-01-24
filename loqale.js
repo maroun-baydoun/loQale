2f95fa7e8f6464a2568bdb63c88c25c750733415
marounbaydoun <maroun.baydoun@gmail.com>
2013-01-24 17:57:07 +0200

    Added use strict

--- loqale.js
+++ loqale.js
@@ -6,127 +6,134 @@
 
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **/
+(function ($) {
 
- (function ($) {
 
+    "use strict";
+    
+    var loQale = (function () {
 
- var loQale=(function() {
-	
+		//the current options of loQale i.e the merging on the defaults and the user-specified options
+        var options;
 		
-		var options;
-		var defaults = {
-			locale: 'en', //the default locale
-			triggerOnInit: false, //if true, a translation will occur after initialization of the library
-			triggerOnLocaleChange: false, //if true, a translation will occur every time the locale is changed via loQale.setLocale
-			context: $('body'), //the default context under which elements will be translated
-			stringAttribute:'data-string' // the data attribute used to identify the string corresponding to a DOM element
-		};
-		
-		
-		function init(userOptions){
-			options=$.extend({}, defaults, userOptions);
-			
-			if (options.triggerOnInit) {
-				translate();
-			}
-		}
-		
-		function getLocalizedString(string, locale) {
-			var locale = locale || options.locale;
-			if (!this.loQale[locale] || !this.loQale[locale].strings) {
-				return string;
-			}
-			return this.loQale[locale].strings[string] || string;
-		};
+		//the default options
+        var defaults = {
+            locale: 'en', //the default locale
+            triggerOnInit: false, //if true, a translation will occur after initialization of the library
+            triggerOnLocaleChange: false, //if true, a translation will occur every time the locale is changed via loQale.setLocale
+            context: $('body'), //the default context under which elements will be translated
+            stringAttribute: 'data-string' // the data attribute used to identify the string corresponding to a DOM element
+        };
 
-		
-		function setLocale(locale) {
-	
-			if(options.locale === locale){
-				return;
-			}
-			
-			options.locale = locale;
+		/**
+		**Initializes loQale. If triggerOnInit is set to true, a translation will occur.
+		**@param {userOptions}
+		**/
+        function init(userOptions) {
+            options = $.extend({}, defaults, userOptions);
 
-			if (options.triggerOnLocaleChange) {
-				translate();
-			}
-		};
+            if (options.triggerOnInit) {
+                translate();
+            }
+        }
 
-		function getLocale() {
-			return options.locale;
-		};
-	
-		function translate(context) {
+		/**
+		**Returns a localized string for the current locale, or the specified locale if specified
+		**/
+        function getLocalizedString(string, locale) {
+            var locale = locale || options.locale;
+            if (!loQale[locale] || !loQale[locale].strings) {
+                return string;
+            }
+            return loQale[locale].strings[string] || string;
+        };
 
-			if(options.onBeforeTranslate){
-				options.onBeforeTranslate();
-			}
-			
-			var elementsToTranslate;
 
-			if (!context) {
-				context = options.context;
-			}
+        function setLocale(locale) {
 
-			elements= context.find('['+options.stringAttribute+']');
+            if (options.locale === locale) {
+                return;
+            }
 
-			for (var i = 0, length = elements.length; i < length; i++) {
+            options.locale = locale;
 
-				var element = $(elements[i]);
-				var elementStringName = element.attr(options.stringAttribute);
-				var elementStringValue = getLocalizedString(elementStringName);
-				var elementTag = element.prop('tagName').toLowerCase();
+            if (options.triggerOnLocaleChange) {
+                translate();
+            }
+        };
 
-				if(options.onBeforeElementTranslate){
-				
-					var returnValue=options.onBeforeElementTranslate(element,elementStringName,elementStringValue);
-					if(typeof returnValue !=='undefined'){
-						if(returnValue===false){
-							continue;
-						}
-						else if(returnValue!==true){
-							elementStringValue=returnValue;
-						}
-					}
-				}
+        function getLocale() {
+            return options.locale;
+        };
 
-				if (elementTag === 'input') {
-					element.attr('value', elementStringValue);
-				} else {
-					element.html(elementStringValue);
-				}
-				
-				if(options.onAfterElementTranslate){
-					options.onAfterElementTranslate(element,elementStringName,elementStringValue);
-				}
+        function translate(context) {
 
-			}
+            if (options.onBeforeTranslate) {
+                options.onBeforeTranslate();
+            }
 
-			if(options.onAfterTranslate){
-				options.onAfterTranslate();
-			}
-		}
-		
-		
-		return { 
-			init: function (userOptions) {
-				init(userOptions);
-			},
-			setLocale:function(locale){
-				setLocale(locale);
-			},
-			getLocale:function(){
-				return getLocale();
-			},
-			getLocalizedString:function(string,locale){
-				return getLocalizedString(string,locale);
-			}
-			
-		};
-		
-	
-	})();
-	
-	window.loQale=loQale;
+            
+            if (!context) {
+                context = options.context;
+            }
+
+            var elementsToTranslate= context.find('[' + options.stringAttribute + ']');
+
+            for (var i = 0, length = elementsToTranslate.length; i < length; i++) {
+
+                var element = $(elementsToTranslate[i]);
+                var elementStringName = element.attr(options.stringAttribute);
+                var elementStringValue = getLocalizedString(elementStringName);
+                var elementTag = element.prop('tagName').toLowerCase();
+
+                if (options.onBeforeElementTranslate) {
+
+                    var returnValue = options.onBeforeElementTranslate(element, elementStringName, elementStringValue);
+                    if (typeof returnValue !== 'undefined') {
+                        if (returnValue === false) {
+                            continue;
+                        } else if (returnValue !== true) {
+                            elementStringValue = returnValue;
+                        }
+                    }
+                }
+
+                if (elementTag === 'input') {
+                    element.attr('value', elementStringValue);
+                } else {
+                    element.html(elementStringValue);
+                }
+
+                if (options.onAfterElementTranslate) {
+                    options.onAfterElementTranslate(element, elementStringName, elementStringValue);
+                }
+
+            }
+
+            if (options.onAfterTranslate) {
+                options.onAfterTranslate();
+            }
+        }
+
+
+        return {
+            init: function (userOptions) {
+                init(userOptions);
+            },
+            setLocale: function (locale) {
+                setLocale(locale);
+            },
+            getLocale: function () {
+                return getLocale();
+            },
+            getLocalizedString: function (string, locale) {
+                return getLocalizedString(string, locale);
+            }
+
+        };
+
+
+    })();
+
+    window.loQale = loQale;
 })(jQuery);
